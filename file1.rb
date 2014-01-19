30a0f7770ec8543da75843916638cca14f349ec6
Evan Fader <efader@brandeis.edu>
2014-01-18 22:44:22 -0500

    methods for loading data and popularities

--- file1.rb
+++ file1.rb
@@ -1,1 +1,52 @@
-#sdfhnsdkjfnbskjdhfbshjfbshdjfbsjdhfsd
+class MovieRating	
+	def initialize
+		@raw_data = []
+		@popularity = {}
+	end
+
+	def load_data
+		# puts raw data into an array
+		@raw_data = []
+		File.open('ml-100k/u.data') do |file|
+			file.each_line do |line|
+				@raw_data << line.split.map(&:to_i)
+			end
+		end
+	end
+
+	def load_popularities()
+		# puts popularities into a hash by movie id
+		# popularities are determined by combined total ratings on
+		# a scale from -2 to 2, figuring that a rating of 1 or 2
+		# should detract from a movie's overall popularity
+		# 'popularity' suggests favoring movies that have been
+		# viewed the most in addition to being well liked, so
+		# this system is appropriate
+		@popularity = {}
+		@raw_data.each do |entry|
+			if @popularity[entry[1]]
+				@popularity[entry[1]] += entry[2] - 3
+			else
+				@popularity[entry[1]] = entry[2] - 3
+			end
+		end
+		@popularity = @popularity.sort_by {|movie_id, rating| rating}.reverse
+	end
+
+	def popularity(movie_id)
+
+		return @popularity[movie_id]
+	end
+
+	def popularities
+		return @popularity
+	end
+
+
+end
+
+instance = MovieRating.new
+instance.load_data
+instance.load_popularities
+puts instance.popularities.inspect
+puts instance.popularity(1663)
